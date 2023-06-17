b9d2a3553f644d42346cf90dff5b9b843e63b6ef
Arbaazyaseen15 <arbaazyaseen@gmail.com>
2023-06-17 10:34:12 +0530

    DateTime from import java.time

---
+++ DateTime.java
@@ -0,0 +1,22 @@
+import java.time.LocalDateTime; // Import the LocalDateTime class
+import java.time.format.DateTimeFormatter; // Import the DateTimeFormatter class
+
+public class Main {
+  public static void main(String[] args) {
+    LocalDateTime myDateObj = LocalDateTime.now();
+    System.out.println("Before formatting: " + myDateObj);
+    DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+    String formattedDate = myDateObj.format(myFormatObj);
+    System.out.println("After formatting: " + formattedDate);
+
+    DateTimeFormatter obj = DateTimeFormatter.ofPattern("E, MMM dd yyyy");
+
+    formattedDate = myDateObj.format(obj);
+
+     System.out.println("After formatting: " + formattedDate);
+
+
+
+  }
+}
